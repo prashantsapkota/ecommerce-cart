326d484394faa17d99471fb3ccaa2eb794b42864
Prashant <you@example.com>
2022-08-15 00:10:22 +0545

    ecommerce

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import React from "react";
+
+import Cart from "./Component/Cart";
+import Navbar from "./Component/Navbar";
+
+const App = () => {
+  return (
+    <>
+      <Cart />
+      <Navbar />
+      
+      </>
+    
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
+
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <BrowserRouter>
+  <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+
+);
